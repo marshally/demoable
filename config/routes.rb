beaee87f9aa12a1ea1b5135411893977f5f10249
thebinaryhood <mlohstroh@gmail.com>
2013-06-24 11:08:08 -0500

    working on getting the requesting of the codes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Demoable::Engine.routes.draw do
+  resources :access_codes
   root :to => 'access_codes#index'
 end
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-
   mount Demoable::Engine => "/demoable"
 end
